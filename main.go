7db1b9f3b5656289f57764053677677600397121
Chris <chriscottone1@gmail.com>
2025-07-11 23:36:28 -0400

    made a buffer, read into how syscalls work and the Read() & Socket() methods

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, 1)
+	fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, 3)
 
 	if err != nil {
 		fmt.Printf("Error creating socket: %v.\n", err)
@@ -14,4 +14,9 @@ func main() {
 	defer syscall.Close(fd)
 
 	fmt.Printf("Socket created successfully with file descriptor: %d.\n", fd)
+
+	buffer := make([]byte, 1500)
+	data, err := syscall.Read(fd, buffer)
+
+	fmt.Println(data)
 }
