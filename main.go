524bb79f90c30679e6d08f87bad39eb7448001cb
Chris <chriscottone1@gmail.com>
2025-07-13 00:15:18 -0400

    successfully read raw bytes from ping packet. began building Header struct and initialized test data.

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, 3)
+	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, 1)
 
 	if err != nil {
 		fmt.Printf("Error creating socket: %v.\n", err)
@@ -16,7 +16,15 @@ func main() {
 	fmt.Printf("Socket created successfully with file descriptor: %d.\n", fd)
 
 	buffer := make([]byte, 1500)
-	data, err := syscall.Read(fd, buffer)
+
+	data, err := syscall.Read(fd, buffer) // "data" is how many bytes the packet contains
+	fmt.Println("Data read.")
+
+	if err != nil {
+		fmt.Printf("Could not read data, error: %v\n", err)
+	}
 
 	fmt.Println(data)
+	packet_info := buffer[0:data] // the actual packet bytes are queued in the buffer by the kernel.
+	fmt.Printf("Info contained therein: %v", packet_info)
 }
